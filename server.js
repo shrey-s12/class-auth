e64487553748d43b6983fba6c9d7824d7128e585
Shrey Singhal <shreynbd12@gmail.com>
2025-01-04 13:12:47 +0530

    Token Generate and delete

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const dorenv = require('dotenv');
 dorenv.config();
 
-const bcrypt = require('bcrypt');
 const express = require('express');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cookieParser());
 
+// db
 const users = [];
+// server
+const sessions = new Map();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get("/", authMiddleware, (req, res) => {
+    res.json({ message: `Hello ${req.user.username}` })
 });
 
 app.get("/admin", (req, res) => {
@@ -32,24 +37,41 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
     const { username, password } = req.body;
-
+    const user = users.find(ele => ele.username === username);
+    if (!user) {
+        return res.status(400).json({ message: "User not Found" });
+    }
     try {
-        const user = users.find(ele => ele.username === username);
-        if (!user) {
-            return res.status(400).json({ message: "User not Found" });
-        }
-
         const isMatched = await bcrypt.compare(password, user.password);
         if (!isMatched) {
             return res.status(400).json({ message: "Incorrect Password" });
         }
-
-        return res.json({ message: "User successfully logged in!" })
     } catch (e) {
         return res.status(500).json({ message: "Something went wrong" });
     }
+
+    const sessionId = crypto.randomUUID()
+    sessions.set(sessionId, user);
+
+    res.cookie("sessionId", sessionId);
+
+    return res.json({ message: "User successfully logged in!" })
+});
+
+app.delete("/logout", (req, res) => {
+    sessions.delete(req.cookies.sessionId);
+    res.json({ message: "User successfully logged out" })
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+function authMiddleware(req, res, next) {
+    const user = sessions.get(req.cookies.sessionId);
+    if (!user) {
+        return res.status(400).json({ message: "Unauthorized" })
+    }
+    req.user = user;
+    next();
+}
