8f3f4d11b1949fdd03c89f34c208f5af3a9c7992
Shrey Singhal <shreynbd12@gmail.com>
2025-01-04 11:36:14 +0530

    Password Bcrypt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dorenv = require('dotenv');
 dorenv.config();
 
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
@@ -16,27 +17,38 @@ app.get("/admin", (req, res) => {
     res.send(users);
 });
 
-app.post("/register", (req, res) => {
-    const { username, password } = req.body;
-    const user = { username: username, password: password };
-    users.push(user);
-    res.status(201).json({ message: "User Created successfully" })
+app.post("/register", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const salt = await bcrypt.genSalt();
+        const hasPassword = await bcrypt.hash(password, salt);
+        const user = { username: username, password: hasPassword };
+        users.push(user);
+        res.status(201).json({ message: "User Created successfully" })
+    } catch (e) {
+        return res.status(500).json({ message: "Something went wrong" });
+    }
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
     const { username, password } = req.body;
 
-    const user = users.find(ele => ele.username === username);
-    if (!user) {
-        return res.status(400).json({ message: "User not Found" });
-    }
+    try {
+        const user = users.find(ele => ele.username === username);
+        if (!user) {
+            return res.status(400).json({ message: "User not Found" });
+        }
 
-    if (user.password !== password) {
-        return res.status(400).json({ message: "Incorrect Password" });
-    }
+        const isMatched = await bcrypt.compare(password, user.password);
+        if (!isMatched) {
+            return res.status(400).json({ message: "Incorrect Password" });
+        }
 
-    return res.json({ message: "User successfully logged in!" })
-})
+        return res.json({ message: "User successfully logged in!" })
+    } catch (e) {
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
